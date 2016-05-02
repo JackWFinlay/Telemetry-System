78d800bb0fec9aa3aa148814cfc38695a339539e
Jack Witbrock Finlay <jack.witbrock@gmail.com>
2016-05-02 14:00:43 +1200

    Update.

--- driver.py
+++ driver.py
@@ -2,32 +2,37 @@
 import RPi.GPIO as GPIO
 from datetime import datetime
 
+print GPIO.VERSION
+
 channel = 8
 pulseNormal = 5
 pulseMin = 1
-pulseTimeout = 70
+pulseTimeout = 10
 pulseTimeoutMilliseconds = pulseTimeout * 1000
 
 GPIO.setmode(GPIO.BOARD) # Set library to use board numbering rather than internal numbering of the SOC channels
 GPIO.setup(channel, GPIO.IN) # Set up channel to use for input.
 
-while (true):
-	GPIO.wait_for_edge(channel, GPIO.RISING) # Wait for High signal to start.
-	timeStart = datetime.now() # Gets the current timestamp.
+while (True):
+    print "Waiting for initial edge."
+    something = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=pulseTimeoutMilliseconds) # Wait for High signal to start.
+    timeStart = datetime.now() # Gets the current timestamp.
+    print "Timeout or Rising edge detected: " + timeStart
 
-	GPIO.wait_for_edge(channel, GPIO.RISING, timeout = pulseTimeoutMilliseconds) # Wait for another pulse, timeout after timeout
-	timePulse = datetime.now() # Gets the current timestamp so we can then work out the time difference.
+    print "Waiting for pulse."
+    something = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=pulseTimeoutMilliseconds) # Wait for another pulse, timeout after timeout
+    timePulse = datetime.now() # Gets the current timestamp so we can then work out the time difference.
+    print "Timeout or Pulse detected: " + timePulse
 
-	timeDelta = timePulse - timeStart
-	print timeDelta
+    timeDelta = timePulse - timeStart
+    print timeDelta
 
-	if (timeDelta.seconds > pulseNormal):
-		print "Pulse rate greater than normal."
-		exit(1) # Nagios status 'Warning'
-	elif (timeDelta.seconds < pulseNormal):
-		print "Pulse rate less than normal."
-		exit(1) # Nagios status 'Warning'
-	elif (timeDelta.seconds > pulseTimeout):
-		print "No pulse. Status Unknown"
-		exit(3) # Nagios status 'Unknown'
-
+    if (timeDelta.seconds > pulseNormal):
+        print "Pulse rate greater than normal."
+        exit(1) # Nagios status 'Warning'
+    elif (timeDelta.seconds < pulseNormal):
+        print "Pulse rate less than normal."
+        exit(1) # Nagios status 'Warning'
+    elif (timeDelta.seconds > pulseTimeout):
+        print "No pulse. Status Unknown"
+        exit(3) # Nagios status 'Unknown'
