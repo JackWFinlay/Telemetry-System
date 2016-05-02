0dc56420335bce741ce646ea77ba5a74dd2b6215
Jack Witbrock Finlay <jack.witbrock@gmail.com>
2016-05-02 15:54:04 +1200

    Add sleep for debounce

--- driver.py
+++ driver.py
@@ -1,38 +1,56 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
+import json
+import time
 from datetime import datetime
 
 print GPIO.VERSION
 
-channel = 8
-pulseNormal = 5
-pulseMin = 1
-pulseTimeout = 10
-pulseTimeoutMilliseconds = pulseTimeout * 1000
+with open('availabilityconfig.json') as json_data_file:
+    data = json.load(json_data_file)
+
+channel = data["channel"] # The GPIO channel used for sensing the pulse.
+pulseNormal = data["pulseNormal"] # The normal setting for the pulse rate.
+pulseMin = data["pulseMin"] # The start of the range for timer.
+pulseMax = data["pulseMax"] # The end of the range for timer.
+pulseMaxMilliseconds = pulseMax * 1000
+
+print "Channel: " + str(channel)
+print "pulseNormal: " + str(pulseNormal)
+print "pulseMin: " + str(pulseMin)
+print "pulseMax: " + str(pulseMax)
 
 GPIO.setmode(GPIO.BOARD) # Set library to use board numbering rather than internal numbering of the SOC channels
 GPIO.setup(channel, GPIO.IN) # Set up channel to use for input.
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Bias the floating voltage to 0 when disconnected.
 
 while (True):
     print "Waiting for initial edge."
-    something = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=pulseTimeoutMilliseconds) # Wait for High signal to start.
+    something = GPIO.wait_for_edge(channel, GPIO.RISING)#, timeout=pulseMaxMilliseconds) # Wait for High signal to start.
     timeStart = datetime.now() # Gets the current timestamp.
-    print "Timeout or Rising edge detected: " + timeStart
+    print "Timeout or Rising edge detected: " + str(timeStart)
+    
+    print "Sleeping for " + str(pulseMin) + "seconds (pulseMin)."
+    time.sleep(pulseMin); # Sleep as no pulse is expected for pulseMin seconds. Also prevents switch bounce.
+
 
     print "Waiting for pulse."
-    something = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=pulseTimeoutMilliseconds) # Wait for another pulse, timeout after timeout
+    something = GPIO.wait_for_edge(channel, GPIO.RISING)#, timeout=pulseMaxMilliseconds) # Wait for another pulse, timeout after timeout
     timePulse = datetime.now() # Gets the current timestamp so we can then work out the time difference.
-    print "Timeout or Pulse detected: " + timePulse
+    print "Timeout or Pulse detected: " + str(timePulse)
 
     timeDelta = timePulse - timeStart
-    print timeDelta
+    print "Pulse rate: " + str(timeDelta)
 
-    if (timeDelta.seconds > pulseNormal):
+    if (timeDelta.seconds < pulseMin):
+        print "Pulse less than minimum. Error detected."
+        exit(2) # Nagios status 'critical'. Something is really wrong if pulse is faster than the range specified.
+    elif (timeDelta.seconds > pulseNormal):
         print "Pulse rate greater than normal."
         exit(1) # Nagios status 'Warning'
     elif (timeDelta.seconds < pulseNormal):
         print "Pulse rate less than normal."
         exit(1) # Nagios status 'Warning'
-    elif (timeDelta.seconds > pulseTimeout):
+    elif (timeDelta.seconds > pulseMax):
         print "No pulse. Status Unknown"
-        exit(3) # Nagios status 'Unknown'
+        exit(3) # Nagios status 'Unknown' No pulse detected in usual range. Return unkown
