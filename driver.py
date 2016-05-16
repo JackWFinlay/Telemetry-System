094d5d38bf7793b07b55a4a9fbedceb25d23a5e2
Jack Witbrock Finlay <jack.witbrock@gmail.com>
2016-05-16 12:36:57 +1200

    Update

--- driver.py
+++ driver.py
@@ -5,9 +5,9 @@ import time
 import sys
 from datetime import datetime
 
-print GPIO.VERSION
+#print GPIO.VERSION
 
-with open('config.json') as config:
+with open("/usr/local/nagios/etc/objects/config.json") as config:
     data = json.load(config)
 
 timer                = sys.argv[1]; # Select which timer to run script for.
@@ -19,31 +19,31 @@ tolerance            = data[timer]["tolerance"] # tolerance for timer.
 
 pulseMaxMilliseconds = ( pulseMax + 1 )* 1000 # Make timeout 1 second longer than the max expected pulse wait.
 
-print "Channel: "     + str(channel)
-print "pulseNormal: " + str(pulseNormal)
-print "pulseMin: "    + str(pulseMin)
-print "pulseMax: "    + str(pulseMax)
-print "tolerance: "   + str(tolerance)
+#print "Channel: "     + str(channel)
+#print "pulseNormal: " + str(pulseNormal)
+#print "pulseMin: "    + str(pulseMin)
+#print "pulseMax: "    + str(pulseMax)
+#print "tolerance: "   + str(tolerance)
 
 GPIO.setmode(GPIO.BOARD) # Set library to use board numbering rather than internal numbering of the SOC channels
 GPIO.setup(channel, GPIO.IN) # Set up channel to use for input.
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Bias the floating voltage to 0 when disconnected.
 
-print "Waiting for initial edge."
+#print "Waiting for initial edge."
 something = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=pulseMaxMilliseconds) # Wait for High signal to start.
 timeStart = datetime.now() # Gets the current timestamp.
-print "Timeout or Rising edge detected: " + str(timeStart)
+#print "Timeout or Rising edge detected: " + str(timeStart)
 
-print "Sleeping for " + str(pulseMin - 1) + " seconds (i.e. pulseMin - 1)."
-time.sleep(pulseMin - 1); # Sleep as no pulse is expected for pulseMin seconds. Also prevents switch bounce.
+#print "Sleeping for " + str(pulseMin - 1) + " seconds (i.e. pulseMin - 1)."
+time.sleep(0.01); # Sleep as no pulse is expected for pulseMin seconds. Also prevents switch bounce.
 
-print "Waiting for pulse."
+#print "Waiting for pulse."
 something = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=pulseMaxMilliseconds) # Wait for another pulse, timeout after timeout
 timePulse = datetime.now() # Gets the current timestamp so we can then work out the time difference.
-print "Timeout or Pulse detected: " + str(timePulse)
+#print "Timeout or Pulse detected: " + str(timePulse)
 
 timeDelta = timePulse - timeStart
-print "Pulse rate: " + str(timeDelta)
+#print "Pulse rate: " + str(timeDelta)
 
 if ((timeDelta.seconds > pulseNormal - tolerance) and (timeDelta.seconds < pulseNormal + tolerance)): # Check pulse time is normal and within tolerance
     print "Pulse rate normal."
--- gpio_input.py
+++ gpio_input.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
-channel = 5
+import sys
+
+channel = sys.argv[1]
 
 GPIO.setmode(GPIO.BOARD) # Set library to use board numbering rather than internal numbering of the SOC channels
 GPIO.setup(channel, GPIO.IN) # Set up channel to use for input.
 
 
 # while True:
-time.sleep(1)
 generatorStatus = GPIO.input(channel) # Read value of the GPIO channel. At this stage I believe 1 is standby, 0 running.
     
 if (generatorStatus):
